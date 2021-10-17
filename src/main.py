04bdc1776327d1c1704cfdb5cd1895baa422addd
david.spavor <dadkos34@gmail.com>
2021-10-17 17:09:51 +0200

    added initialize mongo

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 from parser.parser import parse
 from download.cache import cache
 from download.download import download
+from mongo.mongo import initialize_mongo
 import os
 
 def main():
@@ -12,8 +13,11 @@ def main():
     #path from makefile so if you want to run this src/main.py
     path_to_data = os.path.abspath(os.path.join(".","data")) 
 
+    #download data
     list_of_files_to_be_download = cache(path_to_data) 
     download(list_of_files_to_be_download,path_to_data)
 
+    #initialize mongo
+    initialize_mongo()
 if __name__ == "__main__":
     main()
