941d3dd4e071623bd13715dc02dc7ea7a5423603
Martin Osvald <m.osvald9913@gmail.com>
2021-10-30 23:03:42 +0200

    added dynamic downloading for citizen count

--- src/download/download.py
+++ src/download/download.py
@@ -1,18 +1,27 @@
 # author: Martin Osvald
-from download.constants import  URL
 import requests
 import os
 
-def download(to_be_downloaded: list, where: str):
+def download(to_be_downloaded: list, where: str , name=None):
     """
     Function for downloading datasets from URL constans
     """
 
-    for file in to_be_downloaded:
-        csv_url = URL + file
+    for csv_url in to_be_downloaded:
+        
+        # csv_url = URL + file
+
+        if name == None:
+            file = csv_url.split('/')[-1]
+        else: 
+            file = name
+
+
         print("Downloading ", csv_url)
         req = requests. get(csv_url)
         url_content = req.content
+
+
         csv_file = open( os.path.join(where,file), 'wb')
         csv_file.write(url_content)
         csv_file.close()
