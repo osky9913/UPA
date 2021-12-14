06361a90f15c6dc2bd22dff3379b08636f1f69e1
dowl <xpatek08@stud.fit.vutbr.cz>
2021-12-14 19:42:56 +0100

    added next query and plot (comparision of vacc/infected people)

--- src/mongo/mongo.py
+++ src/mongo/mongo.py
@@ -1,27 +1,30 @@
 from pymongo import MongoClient
-import os, json, pandas
+import os
+import json
+import pandas
 from pymongo.database import Database
 
 
-#array of csv files, which will be imported to mongodb
-csv_collection_names = ["orp",
-                        "obce"]
+# array of csv files, which will be imported to mongodb
+csv_collection_names = ["osoby", "ockovani-zakladni-prehled"]
 
-def import_csv_data(db : Database):
+
+def import_csv_data(db: Database):
 
     list_of_col = db.list_collection_names()
-    #if db contains collections, drop them
+    # if db contains collections, drop them
     if(list_of_col != 0):
         for col in list_of_col:
             db.drop_collection(col)
 
     for col_name in csv_collection_names:
-        #create collections 
+        # create collections
         mycol = db[col_name]
-        #insert data 
+        # insert data
         data = pandas.read_csv(os.path.join("data", col_name + ".csv"))
         payload = json.loads(data.to_json(orient='records'))
         mycol.insert(payload)
+
 
 def initialize_mongo():
     """Creates new instance of Mongo database and returns it.
@@ -37,7 +40,7 @@ def import_collection(db: Database, collection_name, data):
     Does nothing when db is null or name is empty.
     """
 
-    if db is None or collection_name == "" :
+    if db is None or collection_name == "":
         return
 
     if collection_name in db.list_collection_names():
